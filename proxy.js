c3f0a2a09132fcff0084ec6ffea620997dcb7c61
Francesco Delfino <delfinof@gmail.com>
2011-12-15 23:08:27 +0100

    correcting the client protocol

--- proxy.js
+++ proxy.js
@@ -24,14 +24,15 @@ var proxyapi = {
     "/connect/fb":  function (request, response, application, urlObj, queryObj, call_usertoken) {
         if ( application.facebook != undefined ) { 
             var fb_id = application.facebook.app_id;
+            var fb_secret =  application.facebook.secret;
             var fb_callback = application.facebook.callback || "http://apic.musixmatch.com/fb/callback";
             var fb_scope= application.facebook.scope || "email,offline_access";
-	    var fb_display = application.facebook.display || "popup";
+	        var fb_display = application.facebook.display || "popup";
 
             response.writeHeader(302, {
                 'Location': 'https://www.facebook.com/dialog/oauth?client_id=' + fb_id   
                      + "&redirect_uri=" + encodeURIComponent(fb_callback) 
-                     + "&scope=" + fb_scope + "&display=" + fb_display,
+                     + "&scope=" + fb_scope + "&display=" + fb_display + "&response_type=token" ,
                 'Content-Type': 'text/plain; charset=utf-8',
                 'x-mxm-cache': 'no-cache'
             });
@@ -343,8 +344,56 @@ var proxyRequest = function (request_url, request, response, app_headers ) {
     }
 }
 
-var checkFacebookToken = function (facebook_application, facebook_token, ok_callback, nok_callback) {
 
+var createFacebookToken = function(facebook, code, ok_callback, nok_callback) {
+    var fb_id = facebook.app_id;
+    var fb_secret = facebook.secret;
+    var fb_callback = facebook.callback || "http://apic.musixmatch.com/fb/callback";
+    var fb_scope= facebook.scope || "email,offline_access";
+	var fb_display = facebook.display || "popup";
+
+    var options = {
+        host: 'graph.facebook.com',
+        port: 443,
+        path: '/oauth/access_token?code=' + code+
+            "&client_id=" + fb_id +
+            "&client_secret=" + fb_secret +
+            "&redirect_uri=" + fb_callback,
+        agent: false,
+        method: 'GET'
+    };
+
+    var req = https.request(options, function (client_response) {
+        try {
+            if (debug) console.log("connected to facebook");
+            var body = '';
+            client_response.on('data', function (data) {
+                if (debug) console.log(".");
+                body += data;
+            });
+            client_response.on('end', function () {
+                ok_callback( body );
+            });
+        }
+        catch (e) {
+            if (debug) console.log(e);
+            nok_callback();
+        }
+    });
+
+    req.on('error', function (e) {
+        if (debug) console.log(connectionException);
+        nok_callback();
+    });
+
+    req.end();
+
+
+
+} 
+
+
+var checkFacebookToken = function (facebook_application, facebook_token, ok_callback, nok_callback) {
     var options = {
         host: 'graph.facebook.com',
         port: 443,
@@ -475,18 +524,28 @@ var handleAuthCallback = function ( request, response ) {
         return true;
     }
     else if ( call == "/fb/callback" ) {
-        if ( queryObj["code"] != undefined ) {
-            queryObj["usertoken"] = "fb:" + queryObj["code"];
-        }
-        var request_url = "/callback" + '?' + qs.stringify(queryObj);
-        response.writeHeader(302, {
-            'Location': request_url,
-            'Content-Type': 'text/plain; charset=utf-8',
-            'x-mxm-cache': 'no-cache'
-        });
-        response.write("");
-        response.end();
 
+        if ( queryObj["redirected"] != undefined ) {
+            queryObj["usertoken"] = "fb:" + queryObj["access_token"];
+            
+            var request_url = "/callback" + '?' + qs.stringify(queryObj);
+            response.writeHeader(302, {
+                'Location': request_url,
+                'Content-Type': 'text/plain; charset=utf-8',
+                'x-mxm-cache': 'no-cache'
+            });
+            response.end();
+        }
+        else {
+            response.writeHeader(200, {
+                'Content-Type': 'text/html; charset=utf-8',
+                'x-mxm-cache': 'no-cache'
+            });
+            response.write("<script type='text/javascript'>");
+            response.write("document.location = ('' + document.location ).replace('#','?') + '&redirected=1';");
+            response.write("</script>" );
+            response.end();
+        }
         return true;
     }
     return false;
