4adcb752848e8d06238e54d85b6daa2816610afe
Francesco Delfino <delfinof@gmail.com>
2012-01-13 16:10:37 +0100

    Adding dfxp file generator

---
+++ routes/facebook-video.js
@@ -0,0 +1,46 @@
+util = require("util");
+http = require("http")
+storage = require( "../lib/storage" );
+
+var config= null;
+module.exports.init = function( _config ) {
+    config = _config;
+};
+module.exports.routes =
+{
+    "/ws/1.1/video.subtitle.dfxp.get":  function (request, response, application, urlObj, queryObj, call_usertoken) {
+        request.validateToken(application, call_usertoken,
+            function(data,state) {
+              response.setHeader("Content-Type", "text/plain");
+              response.setHeader("Accept-Ranges", "bytes");
+              video_id = queryObj["video_id"];
+              
+              var options = {
+                host: config.api_host,
+                port: config.api_port,
+                path: '/ws/1.1/video.subtitle.get?apikey=' + application.apikey +'&format=json&subtitle_format=dfxp&video_id=' + video_id
+              };
+              request = http.get(options);
+              data = "";
+              request.on('response', function (res) {
+                res.on('data', function (chunk) {
+                  data = data + chunk;
+                });
+                res.on('end', function () {
+                  subtitle = JSON.parse(data).message.body.subtitle;
+                  pixel_tracking_url = subtitle.pixel_tracking_url;
+                  tracking = " <tracking>" + pixel_tracking_url + "</tracking>\n"
+                  subtitle = subtitle.subtitle_body;
+                  subtitle = [subtitle.slice(0, 147), tracking, subtitle.slice(147)].join('');
+                  response.setHeader('Content-Length', subtitle.length);
+                  response.write(subtitle);
+                  response.end();
+                });
+              });
+              
+            },
+            function(err,state) {
+                response.sendErrorPacket( 401, "renew" );
+            });
+    }
+}
